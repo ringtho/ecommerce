aee4fecdc9e902942f3f24669229b6647c7759e6
Smith Ringtho <sringtho@gmail.com>
2023-06-05 21:08:46 +0300

    Added assets

--- client.js
+++ client.js
@@ -2,6 +2,6 @@ import { createClient } from "@sanity/client"
 
 export default createClient({
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID, 
-  dataset: 'production', 
+  dataset: process.env.NEXT_PUBLIC_DATASET, 
   useCdn: true 
 })
