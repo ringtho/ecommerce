171ff3ad56dffcf476e9a3a76d466764cf7c2ede
Smith Ringtho <sringtho@gmail.com>
2023-06-05 21:45:49 +0300

    Added sanity client

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+import styles from '../styles/globals.css'
+import { Product, FooterBanner, HeroBanner } from '../components'
 
 const Home = () => {
   return (
-    <div>
+    <>
+      <HeroBanner />
 
-    </div>
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div className='products-container'>
+        {['Product 1', 'Product 2'].map((product) => product)}
+      </div>
+
+      <FooterBanner />
+    </>
   )
 }
 
--- client.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@sanity/client"
-
-export default createClient({
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID, 
-  dataset: process.env.NEXT_PUBLIC_DATASET, 
-  useCdn: true 
-})
