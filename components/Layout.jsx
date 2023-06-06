b431aa886825772a6731e38e99d12046baeac442
Smith Ringtho <sringtho@gmail.com>
2023-06-06 22:27:15 +0300

    Added pages router to replace app router

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import Head from 'next/head'
-import { Navbar, Footer } from '.'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div className='layout'>
+    <div className="layout">
       <Head>
         <title>Audiophile</title>
       </Head>
       <header>
         <Navbar />
       </header>
-      <main className='main.container'>
-        Empty
+      <main className="main-container">
+        {children}
       </main>
       <footer>
         <Footer />
       </footer>
-      
     </div>
   )
 }
